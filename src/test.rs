21f949022d9af5d82006f24127de65ee4cdd0279
czar <czar.yobero@gmail.com>
2022-05-28 20:53:30 -0700

    test create method

---
+++ src/test.rs
@@ -0,0 +1,34 @@
+use diesel::pg::PgConnection;
+use diesel::{Connection, ConnectionError};
+
+use dotenv::dotenv;
+
+use std::env;
+
+fn establish_connection() -> Result<PgConnection, ConnectionError> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+    PgConnection::establish(&database_url)
+}
+
+#[cfg(test)]
+mod db_tests {
+    use super::establish_connection;
+    use crate::db::*;
+    use crate::model::*;
+    use crate::schema::events::{self, dsl};
+    use diesel::{ExpressionMethods, QueryDsl, RunQueryDsl};
+
+    #[test]
+    fn new_event_created_is_ok() {
+        let conn = establish_connection().unwrap();
+        let new = NewEvent {
+            description: "CHI (+3) vs DET (-3)".to_owned(),
+            odds: -110,
+        };
+        let event = new.create(&conn).unwrap();
+        assert_eq!(event.odds, -110);
+        assert_eq!(event.description, "CHI (+3) vs DET (-3)".to_owned());
+        let _ = diesel::delete(dsl::events.filter(dsl::id.eq(event.id))).get_result::<Event>(&conn);
+    }
+}
