9100402306fdee25d5942aeec487b16b89d6f86d
czar <czar.yobero@gmail.com>
2022-06-16 10:27:47 -0700

    modify docstring

--- src/form.rs
+++ src/form.rs
@@ -1,1 +1,48 @@
+use chrono::{NaiveDate, NaiveDateTime};
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Deserialize, Serialize)]
+pub struct GameForm {
+    pub home: String,
+    pub away: String,
+    pub start: String,
+}
+
+impl GameForm {
+    pub fn new() -> Self {
+        GameForm {
+            home: "HOME".to_owned(),
+            away: "AWAY".to_owned(),
+            start: "1987-10-03T17:00:00".to_owned(),
+        }
+    }
+
+    /// Converts `start` member from `String` and returns a `NaiveDateTime` object
+    pub fn start_to_naive(&self) -> NaiveDateTime {
+        let start = self.start.as_bytes();
+        let (yr, mo, dy, hr, mn) = (
+            String::from_utf8(start[0..4].to_vec())
+                .unwrap()
+                .parse::<i32>()
+                .unwrap(),
+            String::from_utf8(start[5..7].to_vec())
+                .unwrap()
+                .parse::<u32>()
+                .unwrap(),
+            String::from_utf8(start[8..10].to_vec())
+                .unwrap()
+                .parse::<u32>()
+                .unwrap(),
+            String::from_utf8(start[11..13].to_vec())
+                .unwrap()
+                .parse::<u32>()
+                .unwrap(),
+            String::from_utf8(start[14..16].to_vec())
+                .unwrap()
+                .parse::<u32>()
+                .unwrap(),
+        );
+
+        NaiveDate::from_ymd(yr, mo, dy).and_hms(hr, mn, 0)
+    }
+}
