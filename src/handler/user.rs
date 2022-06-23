e36f695690feeb7b3e56325023aff03a1446abc0
czar <czar.yobero@gmail.com>
2022-06-23 14:38:48 -0700

    remove unused imports

--- src/handler/user.rs
+++ src/handler/user.rs
@@ -1,38 +1,11 @@
 //! Module for proccessing HTTP requests
 use super::DbPool;
-use crate::db::Retrievable;
 use crate::form::LoginForm;
-use crate::model::user::User;
 use handlebars::Handlebars;
 
 use actix_web::{get, post, web, HttpRequest, HttpResponse, Responder};
 use serde_json::json;
 
-//#[post("/login")]
-//async fn login(
-//pool: web::Data<DbPool>,
-//hb: web::Data<Handlebars<'_>>,
-//form: web::Form<LoginForm>,
-//) -> impl Responder {
-//let conn = pool.get().expect("Could not get connection.");
-//web::block(move || form.0.validate(&conn))
-//.await
-//.map(|_| {
-//let body = hb
-//.render(
-//"success",
-//&json!({"message": "login succesful", "redirect": "/events "}),
-//)
-//.unwrap();
-//HttpResponse::Ok().body(body)
-//})
-//.map_err(|e| {
-//let body = hb
-//.render("login", &json!({"message": e.to_string() }))
-//.unwrap();
-//HttpResponse::Ok().body(body)
-//})
-//}
 #[post("/login")]
 async fn login(
     pool: web::Data<DbPool>,
